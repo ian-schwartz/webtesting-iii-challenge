ec2793d41f549862bda1a608f9661dcc03019fc3
Ian Schwartz <ian.schwartz23@gmail.com>
2019-10-14 14:59:42 -0700

    webtesting-iii mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -6,5 +6,24 @@ test('Controls renders correctly', () => {
     render(<Controls />);
 })
 
+test('Controls render buttons to toggle locked and closed', () => {
+    const { getByTestId } = render(<Controls />);
+
+    getByTestId(/lock-button/i);
+    getByTestId(/close-button/i);
+});
+
+test('Close button disabled if gate is locked', () => {
+    const { getByTestId } = render(<Controls locked={true} />);
+
+    expect(getByTestId(/close-button/i).disabled).toBe(true);
+});
+
+test('Lock button disabled if gate is open', () => {
+    const { getByTestId } = render(<Controls closed={false} />);
+
+    expect(getByTestId(/lock-button/i).disabled).toBe(true);
+});
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
-
 import Dashboard from './dashboard/Dashboard';
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
